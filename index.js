c4d5887b2f2089af0c1d7097127da061b0c267ba
Michael Davis <jrmdd1496@gmail.com>
2022-03-28 11:52:44 -0500

    commit tree 802601ded046ee68bbed04419279b9f34a22b035

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function isWin (game) {
+    return (game.result === 'W');
+}
+
+function superbowlWin (games) {
+    if (games.find(isWin) === undefined) {
+        return undefined;
+    }
+    else{
+        return (games.find(isWin)).year;
+    }
+}
+
+const q = [{year: '2', result: 'L'}, {year: '1', result: 'L'}];
+
+console.log(superbowlWin(q));
+console.log(q[0])
